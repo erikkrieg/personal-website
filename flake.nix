b93e7b0b2aa50181473e33b325128362c0595778
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-08-20 00:08:40 -0400

    sets up nix dev shell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Development shell";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            nodejs_18
+          ];
+        };
+      }
+    );
+}
