2aafea4a2df7846ba9375693a22c962ef0dd33b2
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-08-20 02:33:00 -0400

    makes some vite defaults more obvious by setting them explicitly

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from "vite";
 import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [viteCompression()],
+  plugins: [
+    viteCompression({
+      // Might want to delete origin file when building to deploy to S3
+      deleteOriginFile: false,
+      filter: /\.(js|mjs|json|css|html)$/i,
+    }),
+  ],
 });
