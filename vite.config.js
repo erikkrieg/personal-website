67f8f9a850405380202ce734010513a5dd0250d7
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-08-20 03:00:38 -0400

    compress images

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,38 @@
 import { defineConfig } from "vite";
 import viteCompression from "vite-plugin-compression";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   plugins: [
+    // Defaults suggested in docs
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        progressive: true,
+        quality: 80,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
     viteCompression({
       // Might want to delete origin file when building to deploy to S3
       deleteOriginFile: false,
